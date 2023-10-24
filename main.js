f951e1d0c3562af4d411d286355833420a7af032
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-10-24 13:37:06 -0600

    <build>[optional_scope]: <add hover functionality. clean up js>
    [optional_body]
    
    [optional_footer]

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-// const divArray = []
+const header = document.createElement('div');
 const title = document.createElement('h1');
+header.classList.add('header');
 title.textContent = 'Etch-A-Sketch';
-document.body.append(title)
 
+const content = document.createElement('div');
 
 const container = document.createElement('div');
-document.body.append(container);
 container.classList.add('etch-pad');
 
+//append elements
+document.body.append(header);
+document.body.append(content);
+content.append(container);
+header.append(title);
 
-
-for(let i = 1 ; i < 17; i++){
+for(let i = 1 ; i <= 16; i++){
     const newDiv = document.createElement('div');
-    // divArray.push(newDiv);
     container.append(newDiv)
     newDiv.classList.add(`div${i}`)
     newDiv.classList.add('grid')
 
-    // console.log('div made!')
-    // console.log(i)
+    newDiv.addEventListener('mouseover', () => {
+        newDiv.classList.add('darken')
+    })
 }
+
+
 
 
