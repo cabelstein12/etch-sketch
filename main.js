7f23c09db6071184f972e6b54c4f043beef05aa4
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-10-24 14:39:51 -0600

    <build>[optional_scope]: <add clear button & functionality. add slider for sizing pixels,  no functionality added yet >
    [optional_body]
    
    [optional_footer]

--- main.js
+++ main.js
@@ -4,15 +4,34 @@ header.classList.add('header');
 title.textContent = 'Etch-A-Sketch';
 
 const content = document.createElement('div');
+content.classList.add('content');
 
 const container = document.createElement('div');
 container.classList.add('etch-pad');
 
+const buttonBar = document.createElement('div');
+buttonBar.classList.add('button-bar');
+
+const pixelSize = document.createElement('input');
+pixelSize.classList.add('pixel-size');
+pixelSize.type = 'range';
+
+
+const clearBtn = document.createElement('button');
+clearBtn.textContent = 'Clear Screen'; 
+clearBtn.classList.add('clear-btn');
+
+
 //append elements
 document.body.append(header);
 document.body.append(content);
-content.append(container);
+document.body.append(buttonBar);
+
 header.append(title);
+content.append(container);
+buttonBar.append(pixelSize);
+buttonBar.append(clearBtn);
+
 
 for(let i = 1 ; i <= 16; i++){
     const newDiv = document.createElement('div');
@@ -20,10 +39,25 @@ for(let i = 1 ; i <= 16; i++){
     newDiv.classList.add(`div${i}`)
     newDiv.classList.add('grid')
 
+    
+    newDiv.addEventListener('click', () => {
+        drawEnable = true;
+    })
+
     newDiv.addEventListener('mouseover', () => {
-        newDiv.classList.add('darken')
+
+        newDiv.classList.add('darken');
+        
     })
 }
+
+const gridElements = document.querySelectorAll('.grid');
+
+clearBtn.addEventListener('click', () => {
+    gridElements.forEach((element) => {
+        element.classList.remove('darken');
+    })
+})
 
 
 
