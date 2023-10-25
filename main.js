fd44d851e834557a6ac7afbd5fe08380c3236e02
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-10-24 20:41:48 -0600

    <build>[optional_scope]: <start slider function- ality , convert sections to functions.>
    [optional_body]
    
    [optional_footer]

--- main.js
+++ main.js
@@ -14,14 +14,26 @@ buttonBar.classList.add('button-bar');
 
 const pixelSize = document.createElement('input');
 pixelSize.classList.add('pixel-size');
-pixelSize.type = 'range';
+
+Object.assign(pixelSize, {
+    type: 'range',  // pixelSize.type = 'range';
+    min: 16,        // pixelSize.min = 16;
+    step: 16,       // pixelSize.step = 16;
+    max: 96,
+    value: 16         // pixelSize.max = 96;
+})
+
 
 
 const clearBtn = document.createElement('button');
 clearBtn.textContent = 'Clear Screen'; 
 clearBtn.classList.add('clear-btn');
 
-
+clearBtn.addEventListener('click', () => {
+    gridElements.forEach((element) => {
+        element.classList.remove('darken');
+    })
+})
 //append elements
 document.body.append(header);
 document.body.append(content);
@@ -32,32 +44,33 @@ content.append(container);
 buttonBar.append(pixelSize);
 buttonBar.append(clearBtn);
 
-
-for(let i = 1 ; i <= 16; i++){
-    const newDiv = document.createElement('div');
-    container.append(newDiv)
-    newDiv.classList.add(`div${i}`)
-    newDiv.classList.add('grid')
-
-    
-    newDiv.addEventListener('click', () => {
-        drawEnable = true;
-    })
-
-    newDiv.addEventListener('mouseover', () => {
-
-        newDiv.classList.add('darken');
-        
-    })
+function updatePixelSize(){
+    currentPixelSize = parseInt(pixelSize.value, 10);
+    createGrid();
 }
 
+pixelSize.addEventListener('input', updatePixelSize);
+
+
+function createGrid(){
+      container.innerHTML = '';
+    for(let i = 1 ; i <= currentPixelSize; i++){
+        const newDiv = document.createElement('div');
+        container.append(newDiv)
+        newDiv.classList.add(`div${i}`)
+        newDiv.classList.add('grid')
+        
+        newDiv.addEventListener('mouseover', () => {
+            newDiv.classList.add('darken');
+            
+        })
+    }
+}
+
+let currentPixelSize = 16
 const gridElements = document.querySelectorAll('.grid');
 
-clearBtn.addEventListener('click', () => {
-    gridElements.forEach((element) => {
-        element.classList.remove('darken');
-    })
-})
+createGrid();
 
 
 
